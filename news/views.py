f918e68708be3eb2ea8ad4a32719bcb4d9100b5d
miho s <>
2025-12-03 19:56:51 +0900

    date

--- news/views.py
+++ news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import News
 
 def news_list(request):
-    news_list = News.objects.order_by('-created_at')
+    news_list = News.objects.order_by('-date')
     return render(request, 'news/list.html', {'news_list': news_list})
 
 def news_detail(request, pk):
@@ -10,7 +10,7 @@ def news_detail(request, pk):
     return render(request, 'news/detail.html', {'news': news})
 
 def index(request):
-    recent_news = News.objects.order_by('-published_at')[:4]
+    recent_news = News.objects.order_by('-date')[:4]
     return render(request, 'news/index.html', {
         'recent_news': recent_news,
     })
