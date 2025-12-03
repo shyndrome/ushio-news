2f95430476fdc8b6dc16ce84b9dd0fa749bc8812
miho s <>
2025-12-03 20:08:51 +0900

    html to render

--- news/views.py
+++ news/views.py
@@ -3,14 +3,14 @@ from .models import News
 
 def news_list(request):
     news_list = News.objects.order_by('-date')
-    return render(request, 'news/list.html', {'news_list': news_list})
+    return render(request, 'news/news_list.html', {'news_list': news_list})
 
 def news_detail(request, pk):
     news = get_object_or_404(News, pk=pk)
-    return render(request, 'news/detail.html', {'news': news})
+    return render(request, 'news/news_detail.html', {'news': news})
 
 def index(request):
     recent_news = News.objects.order_by('-date')[:4]
-    return render(request, 'news/index.html', {
+    return render(request, 'news/news_index.html', {
         'recent_news': recent_news,
     })
