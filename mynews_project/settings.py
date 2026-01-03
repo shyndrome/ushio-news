9b2dc8cd074f60455d9dbcf435156425c996c076
miho s <>
2026-01-03 23:23:57 +0900

    PostgreSQL

--- mynews_project/settings.py
+++ mynews_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,9 +92,8 @@ WSGI_APPLICATION = 'mynews_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
     )
 }
 
